e308bed00474beb8603a67933b6b09bf36753c46
Qifan Guo <qifanguo@gmail.com>
2018-09-10 16:58:54 -0400

    Basic Interface Done.

--- app/src/main/java/com/qifan/personal_quiz/MainActivity.java
+++ app/src/main/java/com/qifan/personal_quiz/MainActivity.java
@@ -2,6 +2,10 @@ package com.qifan.personal_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button true_button=findViewById(R.id.true_button);
+        Button false_button=findViewById(R.id.false_button);
+        true_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast=Toast.makeText(MainActivity.this,"Wrong",Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,250);
+                toast.show();
+            }
+        });
+        false_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast=Toast.makeText(MainActivity.this,"Correct",Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,250);
+                toast.show();
+            }
+        });
     }
 }
